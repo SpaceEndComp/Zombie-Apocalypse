0be0fbbde10fbc6941aa6de2e0ba013b873cf714
ArifDev-coder <achadzainul67@gmail.com>
2025-07-04 12:43:14 +0700

    refactor: change const to let for day variable in main.js

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,7 +1,7 @@
 import { world, system } from "@minecraft/server";
 
 system.runInterval(() => {
-    const day = world.getDay();
+    let day = world.getDay();
     if (day == 2) {
         world.sendMessage(
             "§4§lHari ke-10 dimulai!, zombie berevousi ketahap 2!"
