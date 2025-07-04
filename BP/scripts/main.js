4cd923c0ec209f9df765b3a66c0b98c314869e61
flaxencraft <flaxencraft@gmail.com>
2025-07-04 13:28:00 +0700

    repaired

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,22 +1,16 @@
-import { world, system } from "@minecraft/server";
+import { world, system, MinecraftEffectTypes } from "@minecraft/server";
 
-let day = world.getDay();
-if (day == 2) {
-        world.sendMessage(
-            "§4§lHari ke-10 dimulai!, zombie berevousi ketahap 2!"
-        );
-
-        for (const player of world.getPlayers()) {
-            const zombies = player.dimension.getEntities({
-                type: "minecraft:zombie",
-                maxDistance: 100,
-                location: player.location,
-            });
-
-            for (const zombie of zombies) {
-                zombie.addEffect(MinecraftEffectTypes.strength, 99999, 1);
-                zombie.addEffect(MinecraftEffectTypes.speed, 100, 1);
-                zombie.addEffect(MinecraftEffectTypes.regeneration, 10, 0);
-            }
-        }
+let trigerred = false;
+system.runInterval(() => {
+    const day = world.getDay();
+    if (day == 2 && !trigerred) {
+        world.sendMessage("§l§4Hari ke-2 dimulai!, zombie berevolusi ke tahap 2!");
+        const zombies = world.getDimension("overworld").getEntities({ type: "minecraft:zombie" });
+        zombies.forEach(zombie => {
+            zombie.addEffect(MinecraftEffectTypes.strength, 99999, 1);
+            zombie.addEffect(MinecraftEffectTypes.speed, 100, 1);
+            zombie.addEffect(MinecraftEffectTypes.regeneration, 10, 0);
+        });
+        trigerred = true;
     }
+});
