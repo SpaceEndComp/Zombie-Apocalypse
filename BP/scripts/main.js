a558d7e1fcdfca158d1b5ded72187da520a61e05
ArifDev-coder <achadzainul67@gmail.com>
2025-07-04 13:27:31 +0700

    refactor: streamline zombie evolution message and improve code formatting

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,23 +1,20 @@
-import { world, system } from "@minecraft/server";
+import { world, system, MinecraftEffectTypes } from "@minecraft/server";
 
 let day = world.getDay();
 if (day == 2) {
-        world.sendMessage(
-            "§4§lHari ke-10 dimulai!, zombie berevousi ketahap 2!"
-        );
+    world.sendMessage("§4§lHari ke-10 dimulai!, zombie berevolusi ke tahap 2!");
 
-        for (const player of world.getPlayers()) {
-            const zombies = player.dimension.getEntities({
-                type: "minecraft:zombie",
-                maxDistance: 100,
-                location: player.location,
-            });
+    for (const player of world.getPlayers()) {
+        const zombies = player.dimension.getEntities({
+            type: "minecraft:zombie",
+            maxDistance: 100,
+            location: player.location,
+        });
 
-            for (const zombie of zombies) {
-                zombie.addEffect(MinecraftEffectTypes.strength, 99999, 1);
-                zombie.addEffect(MinecraftEffectTypes.speed, 100, 1);
-                zombie.addEffect(MinecraftEffectTypes.regeneration, 10, 0);
-            }
+        for (const zombie of zombies) {
+            zombie.addEffect(MinecraftEffectTypes.strength, 99999, 1);
+            zombie.addEffect(MinecraftEffectTypes.speed, 100, 1);
+            zombie.addEffect(MinecraftEffectTypes.regeneration, 10, 0);
         }
     }
-}, 0);
+}
