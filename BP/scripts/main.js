9d6426aae82be25bbe03fc4be676c787590c3972
ArifDev-coder <achadzainul67@gmail.com>
2025-07-04 11:22:25 +0700

    tambah zombie attack

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,8 +2,23 @@ import { world, system } from "@minecraft/server";
 
 system.runInterval(() => {
     const day = world.getDay();
-    if (day == 10) {
-        world.sendMessage("§4§lHari ke-10 dimulai!, zombie berevousi ketahap 2!");
-        
+    if (day == 2) {
+        world.sendMessage(
+            "§4§lHari ke-10 dimulai!, zombie berevousi ketahap 2!"
+        );
+
+        for (const player of world.getPlayers()) {
+            const zombies = player.dimension.getEntities({
+                type: "minecraft:zombie",
+                maxDistance: 100,
+                location: player.location,
+            });
+
+            for (const zombie of zombies) {
+                zombie.addEffect(MinecraftEffectTypes.strength, 99999, 1);
+                zombie.addEffect(MinecraftEffectTypes.speed, 100, 1);
+                zombie.addEffect(MinecraftEffectTypes.regeneration, 10, 0);
+            }
+        }
     }
 }, 0);
