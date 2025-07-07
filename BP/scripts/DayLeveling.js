ec3ba86a1d73482ccfb3fe8942cb23f14cec3dda
ArifDev-coder <achadzainul67@gmail.com>
2025-07-07 18:05:31 +0700

    Remove BuffZombie From DayLeveling.js

--- BP/scripts/DayLeveling.js
+++ BP/scripts/DayLeveling.js
@@ -18,33 +18,24 @@ export function dayLeveling() {
                         world.sendMessage(
                             `§2Zombie tahap 1 muncul. Belum terlalu berbahaya...`
                         );
-
-                        buffZombies(1);
                     }
 
                     if (day === 10) {
                         world.sendMessage(
                             `§6Zombie tahap 2 berevolusi! Mereka lebih kuat sekarang!`
                         );
-
-                        buffZombies(2);
                     }
 
                     if (day === 50) {
                         world.sendMessage(
                             `§cZombie tahap 3 telah muncul! AWAS, mereka brutal!`
                         );
-
-                        buffZombies(3);
                     }
 
                     if (day % 100 === 0) {
                         world.sendMessage(
                             "§4§lZOMBIE MUTANT TELAH MUNCUL!! BERSIAPLAH!!!"
                         );
-
-                        // randomMob();
-                        spawnMutantZombie();
                     }
 
                     trigerredDays.add(day);
