5992e755d621ea88e42a95088799125daf398ff4
flaxencraft <flaxencraft@gmail.com>
2025-07-21 17:22:18 +0700

    improve daystatus

--- BP/scripts/dayStatus.js
+++ BP/scripts/dayStatus.js
@@ -1,21 +1,20 @@
 import { world} from "@minecraft/server";
 
-// Fungsi untuk menentukan tingkat bahaya berdasarkan hari
-function getDangerLevel(day) {
-    if (day >= 1 && day <= 9) return "§aDamai";
-    if (day >= 10 && day <= 49) return "§eWaspada";
-    if (day >= 50 && day <= 100) return "§cBerbahaya";
-    return "§8Tidak diketahui";
-}
-
 // Fungsi untuk mengirim hari saat player mengetik !days
 export function registerDayStatusCommand() {
     world.beforeEvents.chatSend.subscribe((event, player) => {
         if (event.message.trim().toLowerCase() === "!days") {
             event.cancel = true;
+            let danger = "§aDamai";
             const day = world.getDay();
             const player = event.sender;
-            const danger = getDangerLevel(day);
+            if (day >= 1 && day <= 9) {
+                danger = "§aDamai";
+            } else if (day >= 10 && day <= 49) {
+                danger = "§eWaspada"
+            } else if (day >= 50) {
+                danger = "§4Berbahaya"
+            }
             player.sendMessage(`§aHari ke-${day} di dunia ini!\nTingkat bahaya: ${danger}`);
         }
     });
