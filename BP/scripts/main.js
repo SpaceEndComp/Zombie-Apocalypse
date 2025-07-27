4503280ce3092e068a74518c3e44bfcced1a2cb9
ArifDev-coder <achadzainul67@gmail.com>
2025-07-27 10:37:07 +0700

    comment initializefirefighteraxelogic

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,10 +1,11 @@
 import { dayLeveling } from "./DayLeveling";
 import { world, EffectType, system } from "@minecraft/server";
 import { showStatusUI } from "./ui/ShowStatusUI";
-import { initializeFireFighterAxeLogic } from "./item/meleSystem";
+import { raycastShoot } from "./gun/logic_pistol";
+// import { initializeFireFighterAxeLogic } from "./item/meleSystem";
 
 // memanggil
-initializeFireFighterAxeLogic();
+// initializeFireFighterAxeLogic();
 dayLeveling();
 
 // Initialize the status UI
@@ -110,34 +111,34 @@ system.runInterval(() => {
     }
 }, 5); // setiap detik
 
-
-
-
-
 // Using another interval to remove the weakness effect faster, you can combine it with the other one if you dont mind the slight delay
 system.runInterval(() => {
     for (let player of world.getAllPlayers()) {
-        let item = player.getComponent("equippable").getEquipment("Mainhand")
-        if (item === undefined) continue
-        if (!(Object.keys(itemsConfig).includes(item.typeId))) continue
+        let item = player.getComponent("equippable").getEquipment("Mainhand");
+        if (item === undefined) continue;
+        if (!Object.keys(itemsConfig).includes(item.typeId)) continue;
 
         player.addEffect("weakness", 1, {
             showParticles: false,
-            amplifier: 255
-        })
+            amplifier: 255,
+        });
     }
-})
+});
 
 // The config for each item, you can adjust these as u like
 let itemsConfig = {
     "za:fire_fighter_axe": {
         cd: 0.5,
-        dmg: 8
-    }
-}
-let playersItemCD = {} // keep this empty, but dont remove this line
+        dmg: 8,
+    },
+};
+let playersItemCD = {}; // keep this empty, but dont remove this line
 
-if (world.afterEvents && world.afterEvents.entityDie && world.afterEvents.entityHitEntity) {
+if (
+    world.afterEvents &&
+    world.afterEvents.entityDie &&
+    world.afterEvents.entityHitEntity
+) {
     world.afterEvents.entityDie.subscribe((ev) => {
         if (
             ev.deadEntity.typeId === ZOMBIE_ID &&
@@ -149,31 +150,44 @@ if (world.afterEvents && world.afterEvents.entityDie && world.afterEvents.entity
         }
     });
 
-    world.afterEvents.entityHitEntity.subscribe(event => {
-        let entityHit = event.hitEntity
-        let player = event.damagingEntity
+    world.afterEvents.entityHitEntity.subscribe((event) => {
+        let entityHit = event.hitEntity;
+        let player = event.damagingEntity;
 
-        if (player.typeId != "minecraft:player") return
+        if (player.typeId != "minecraft:player") return;
 
-        let item = player.getComponent("equippable").getEquipment("Mainhand")
-        if (!(Object.keys(itemsConfig).includes(item.typeId))) return
+        let item = player.getComponent("equippable").getEquipment("Mainhand");
+        if (!Object.keys(itemsConfig).includes(item.typeId)) return;
 
-        player.playSound("note.pling", { pitch: 0.7, volume: 0.2 }) // Play sound when a hit is registered, u can remove this for the finsihed product
+        player.playSound("note.pling", { pitch: 0.7, volume: 0.2 }); // Play sound when a hit is registered, u can remove this for the finsihed product
 
-        let currentTime = Date.now()
-        let skip = false
-        if (!playersItemCD[player.nameTag]) { playersItemCD[player.nameTag] = currentTime; skip = true }
+        let currentTime = Date.now();
+        let skip = false;
+        if (!playersItemCD[player.nameTag]) {
+            playersItemCD[player.nameTag] = currentTime;
+            skip = true;
+        }
 
         if (!skip) {
-            let timeDiff = (currentTime - playersItemCD[player.nameTag])/1000
-            if (!(timeDiff >= itemsConfig[item.typeId].cd)) return
-            playersItemCD[player.nameTag] = currentTime
+            let timeDiff = (currentTime - playersItemCD[player.nameTag]) / 1000;
+            if (!(timeDiff >= itemsConfig[item.typeId].cd)) return;
+            playersItemCD[player.nameTag] = currentTime;
         }
 
         system.runTimeout(() => {
-            player.playSound("note.pling", {pitch: 1.5}) // This line also plays a sound when it deals dmg to the target, you can also remove this in the finale product
-            if (!entityHit) return
-            entityHit.applyDamage(8, { cause: "entityAttack", damagingEntity: player })
-        }, itemsConfig[item.typeId].cd*20)
-    })
+            player.playSound("note.pling", { pitch: 1.5 }); // This line also plays a sound when it deals dmg to the target, you can also remove this in the finale product
+            if (!entityHit) return;
+            entityHit.applyDamage(8, {
+                cause: "entityAttack",
+                damagingEntity: player,
+            });
+        }, itemsConfig[item.typeId].cd * 20);
+    });
 }
+
+// Register the raycast shoot function for pistol logic
+world.beforeEvents.itemUse.subscribe((e) => {
+    if (e.itemStack.typeId === "minecraft:carrot_on_a_stick") {
+        raycastShoot(e.source);
+    }
+});
