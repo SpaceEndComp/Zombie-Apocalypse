dc5b8f0d6442e014ddb6cb1f4ffe320b6dd71fb1
ArifDev-coder <achadzainul67@gmail.com>
2025-07-27 21:46:33 +0700

    adding level up skills ability player

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -7,6 +7,7 @@ import "./player/chat/chatLokal.js";
 import { initializeHitDelaySystem } from "./item/meleHitDelays.js";
 import "./item/crowbar_logic.js";
 import { initializeCustomStaminaBar } from "./ui/StaminaBar.js";
+import "./player/leveling/PlayerLeveling.js"
 
 // memanggil
 dayLeveling();
