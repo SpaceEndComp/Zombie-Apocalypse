2b88cbc21b4e5dedbc334f5d53931d9c67e0c4f3
ArifDev-coder <achadzainul67@gmail.com>
2025-08-25 14:20:56 +0700

    refactor night fog logic for clarity and performance

--- BP/scripts/system/NightFog.js
+++ BP/scripts/system/NightFog.js
@@ -4,11 +4,9 @@ system.runInterval(() => {
     const time = world.getTimeOfDay();
     const isMidnight = time > 13000 && time < 22000;
 
-    for (const player of world.getPlayers()) {
-        if (isMidnight) {
-            player.runCommand(`fog @a push "custom:dark_fog" "DarkOverride"`);
-        } else {
-            player.runCommand(`fog @a remove "DarkOverride"`);
-        }
+    if (isMidnight) {
+        player.runCommand(`fog @a push "custom:dark_fog" "DarkOverride"`);
+    } else {
+        player.runCommand(`fog @a remove "DarkOverride"`);
     }
 }, 20);
