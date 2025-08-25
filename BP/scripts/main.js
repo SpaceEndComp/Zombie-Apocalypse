87c9efa4ceef964f286626c4191698f044b335e8
ArifDev-coder <achadzainul67@gmail.com>
2025-08-25 13:28:28 +0700

    remove commented import for RandomMobs in main.js

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,7 +2,6 @@ import "./blocks/radio";
 import "./DayLeveling";
 import "./gun/logic_pistol";
 import "./item/meleHitDelays";
-// import "./mobs/RandomMobs";
 import "./player/chat/chatLokal";
 import "./ui/ShowStatusUI";
 import "./system/NightFog";
